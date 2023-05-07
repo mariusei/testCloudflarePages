19b132518fd51b8f83244ed8ba30ebc088ff9b8f
Marius Berge Eide <mariusbe@gmail.com>
2023-05-07 17:41:20 +0200

    Added cookie-based layout-based auth flow

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,19 +1,55 @@
-import type { LayoutServerLoad } from './$types';
 import { SECRET_KEY } from '$env/static/private';
+import type { LayoutServerLoad } from './$types';
+
+// can optionally compare server set cookie to secret key
+//import { SECRET_KEY } from '$env/static/private';
 
 export const load = (async ({cookies, locals}) => {
 
-    // Check if cookie key is set and correct
-    let key: string | undefined;
 
+    let user: string | undefined;
+
+    // // Cookie based method
+    // // here in Layout.server.ts (no middleware):
+
+    // let user_cookie: string | undefined;
+    // try {
+    //     user_cookie = cookies.get('user_key');
+    //     console.log("layout - user from cookie: " + user_cookie)
+
+    //     if (user_cookie && user_cookie === SECRET_KEY) {
+    //         user = user_cookie;
+    //     } else {
+    //         user = undefined;
+    //     }
+
+    //     return {
+    //         user: user,
+    //         cookies_header: cookies.getAll(),
+    //         cookies_local: locals.cookies,
+    //         // convert timestamp from middleware to readable format
+    //         timestamp: new Date(locals.timestamp).toLocaleString()
+    //     }
+    //     
+    // } catch (error) {
+    //     console.error(error)
+    //     return {
+    //         user: null,
+    //         error: error
+    //     }
+    // }
+
+    // Locals/middleware-based method: from hoooks.server.ts
     try {
-        key = locals.user;
-        console.log("layout - Key: " + key)
+        // Did the middleware return a user?
+        user = locals.user;
+        console.log("layout - user from middleware: " + user)
 
         return {
-            user: key,
+            user: user,
             cookies_header: cookies.getAll(),
             cookies_local: locals.cookies,
+            // convert timestamp from middleware to readable format
             timestamp: new Date(locals.timestamp).toLocaleString()
         }
 
