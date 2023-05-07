960db4580925f188eeebfe4b8f8a5c24d4a4cac5
Marius Berge Eide <mariusbe@gmail.com>
2023-05-07 17:41:34 +0200

    storing user data as store in the context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,27 +3,38 @@
     import type { LayoutData } from './$types';
     import { setContext } from 'svelte';
     import { writable } from 'svelte/store';
-    import { onMount } from "svelte";
-      import { browser } from "$app/environment";
   
+    // data:
+    // - user
+    // - timestamp
+    // - error
+    // - cookies_header
+    // - cookies_local
     export let data: LayoutData;
+    
+    // We should be careful with using stores in Layouts - easily become
+    // globals which are then reused between different user sessions...
+    // see https://github.com/sveltejs/kit/discussions/4339
+    // even though we are working on the 'client side' code (.svelte)
+    //
+    // Instead, use contexts,
+
+    const user = writable();
+    $: user.set(data.user);
+    setContext('user', user);
+
+    // Check the console to see if if the timestamp corresponds
+    // to the time the request was made, or if we have a cached
+    // response from the server.
+
+    console.log("data:", data);
+    console.log("Timestamp of request:", data.timestamp);
   
-    // Create a store and update it when necessary...
-    //const user = writable<null | any>(null);
-    //$: user.set(data.user);
-    //$: user = data.user;
+</script>
   
-    // ...and add it to the context for child components to access
-    $: setContext('user', data.user);
-    //setContext('user', null);
-    onMount(() => {
-      console.log("onMount");
-      console.log("browser:", browser);
-      console.log("data:", data);
-    });
-    //console.log("GOT DATA:", JSON.stringify(data));
-  
-  </script>
-  
-<p class="text-white">The request arrived:<br/> {data.timestamp}</p>
+<p class="text-white">
+  Serving a response based on a request that arrived:<br/> 
+  {data.timestamp}
+</p>
+
 <slot/>
